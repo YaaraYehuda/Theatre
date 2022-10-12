dac93d7d36af6b6950d45f10065e9e8884f6a6c5
Yaara Yehuda <yaarasas2@gmail.com>
2022-10-12 17:24:01 +0300

    ..

--- src/cmps/dot-details.jsx
+++ src/cmps/dot-details.jsx
@@ -5,14 +5,19 @@ export const DotDetails = ({dot, closeModal}) => {
     console.log('dot: ', dot)
     const onBookDot = () => {
         dot.isReserved = true
+        closeModal(null)
+    }
+    const onCloseModal = () =>{
+        dot.isSelected = false
+        closeModal(null)
     }
     
     return <section className="dot-details">
-        <span><CloseIcon onClick={()=>closeModal(null)} /></span>
+        <span><CloseIcon onClick={onCloseModal} /></span>
         <h1>Price: {dot.dotPrice}</h1>
         <h4> Row {dot.i + 1} / Seat {dot.j + 1}</h4>
         {dot.isReserved && <span>Seat already reserved</span>}
         {dot.isReserved && <button className="book-btn-reserved">Book</button>}
-        {!dot.isReserved && <button className="book-btn">Book</button>}
+        {!dot.isReserved && <button className="book-btn" onClick={onBookDot}>Book</button>}
     </section>
 }
